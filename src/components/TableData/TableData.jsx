2bf256167533825a363f0d664062f6ae890a607c
Nightowlam <nightowl3.00am@gmail.com>
2022-12-11 03:46:04 +0600

    incompletely done

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -3,8 +3,16 @@ import { HiColorSwatch, HiDotsVertical } from "react-icons/hi";
 
 const TableData = () => {
   const [allData, setAllData] = useState([]);
-
   const [order, setOrder] = useState("ASC");
+  const [color, setColor] = useState("white");
+  const click = (color) => {
+    setColor(color);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = color;
+  }, [color]);
+
   useEffect(() => {
     fetch(`data.json`)
       .then((res) => res.json())
@@ -27,64 +35,65 @@ const TableData = () => {
       setOrder("ASC");
     }
   };
-  const handleBgChange = (e) => {
-    if (e.target) {
-      <HiColorSwatch className="text-red-600" />;
-    }
-  };
 
   return (
-    <table className="table-auto text-center mx-auto border my-24">
-      <thead>
-        <tr>
-          <th className="border w-44">Id</th>
+    <section className="my-24">
+      <h3 className="text-center text-3xl font-mono font-medium mb-10 ">
+        Data Table React Component
+      </h3>
+      <table className="table-auto text-center mx-auto border ">
+        <thead>
+          <tr>
+            <th className="border w-44">Id</th>
 
-          <th className="border w-44 flex items-center justify-between">
-            <span className="ml-12">First name</span>
-            <span className="dropdown dropdown-down dropdown-end">
-              <label tabIndex={0} className="rounded-box w-40 cursor-pointer">
-                <HiDotsVertical className="" />
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu menu-compact dropdown-content mt-3 p-2 bg-base-100 border  w-36"
-              >
-                <li>
-                  <button disabled>Unsort</button>
-                </li>
-                <li>
-                  <button onClick={() => sorting("first_name")}>Sort by ASC</button>
-                </li>
-                <li>
-                  <button onClick={() => sorting("first_name")}>Sort by DESC</button>
-                </li>
-              </ul>
-            </span>
-          </th>
-          <th className="border w-44">Last name</th>
-          <th className="border w-24">Age</th>
-          <th className="border w-72">Full name</th>
-          <th className="border w-24">Action</th>
-        </tr>
-      </thead>
-      <tbody className="border">
-        {allData?.map((d, i) => (
-          <tr key={i} onClick={handleBgChange}>
-            <td>{i + 1}</td>
-            <td>{d.first_name} </td>
-            <td>{d.last_name}</td>
-            <td>{d.age}</td>
-            <td>{d.full_name}</td>
-            <td>
-              {}
-              <button>
-                <HiColorSwatch className="text-green-600" />
-              </button>
-            </td>
+            <th className="border w-44 flex items-center justify-between">
+              <span className="ml-12">First name</span>
+              <span className="dropdown dropdown-down dropdown-end">
+                <label tabIndex={0} className="rounded-box w-40 cursor-pointer">
+                  <HiDotsVertical className="" />
+                </label>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-compact dropdown-content mt-3 p-2 bg-base-100 border  w-36"
+                >
+                  <li>
+                    <button onClick={() => sorting("first_name")}>Sort by ASC</button>
+                  </li>
+                  <li>
+                    <button onClick={() => sorting("first_name")}>Sort by DESC</button>
+                  </li>
+                </ul>
+              </span>
+            </th>
+            <th className="border w-44">Last name</th>
+            <th className="border w-24">Age</th>
+            <th className="border w-72">Full name</th>
+            <th className="border w-24">Action</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="border">
+          {allData?.map((d, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{d.first_name} </td>
+              <td>{d.last_name}</td>
+              <td>{d.age}</td>
+              <td>{d.full_name}</td>
+              <td>
+                {}
+                <button
+                  onClick={() => {
+                    click("yellow");
+                  }}
+                >
+                  <HiColorSwatch className="text-green-600" />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
